13722aaa26dcf51e9aa7d4f45d32bd89cafcb6d7
Vinod Khathi <87168769+vinod-kha@users.noreply.github.com>
2025-10-16 19:54:50 +0530

    Delete DevOps-projects/Microservice-python-app directory

--- DevOps-projects/Microservice-python-app/Helm_charts/Postgres/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE auth_user (
-    id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    email VARCHAR (255) NOT NULL,
-    password VARCHAR (255) NOT NULL
-);
-
---Add Username and Password for Admin User
--- INSERT INTO auth_user (email, password) VALUES ('user-email@gmail.com', '123456');
-INSERT INTO auth_user (email, password) VALUES ('vinodkhathi04@gmail.com', '123456');
